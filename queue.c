yy214123/lab0-c

commit 40f7ff38bd8e78d949f9fd3bdfc64927dafb3657

Implement q_insert_head and q_insert_tail
This commit introduces the implementation of two key operations for managing
a queue: `q_insert_head` and `q_insert_tail`. These functions allow for
inserting elements at the beginning and the end of the queue, respectively.

For `q_insert_head`, after successful string duplication, it adds the new
element to the start of the list using `list_add`. Similarly, `q_insert_tail`
adds the new element to the end of the list using `list_add_tail`. Both
operations ensure the dynamic addition of elements to the queue, enhancing
its functionality and flexibility.

queue.c
@@ -40,13 +40,55 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    element_t *new = malloc(sizeof(element_t));
+    if (!new)
+        return false;
+
+    new->value = strdup(s);
+    if (!new->value) {
+        free(new);
+        return false;
+    }
+    list_add(&new->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    ```c
+bool q_insert_head(struct list_head *head, char *s)
+{
+    element_t *new = malloc(sizeof(element_t));
+    if (!new)
+        return false;
+
+    new->value = strdup(s);
+    if (!new->value) {
+        free(new);
+        return false;
+    }
+    list_add(&new->list, head);
     return true;
+}
+
+/* Insert an element at tail of queue */
+bool q_insert_tail(struct list_head *head, char *s)
+{
+    element_t *new = malloc(sizeof(element_t));
+    if (!new)
+        return false;
+
+    new->value = strdup(s);
+    if (!new->value) {
+        free(new);
+        return false;
+    }
+    list_add_tail(&new->list, head);
+    return true;
+}
+
+```
 }
 
 /* Remove an element from head of queue */

commit 6d5be2f6ece7b0722702cbddff5678dadf91654b

Remove duplicate declarations of q_insert_head and q_insert_tail
This commit resolves an issue introduced in the previous commit (40f7ff3),
where `q_insert_head` and `q_insert_tail` were inadvertently declared
twice. The duplicate declarations have been removed to ensure the code
base remains clean and to avoid compilation errors related to redefinition.

queue.c
@@ -56,25 +56,6 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
-    ```c
-bool q_insert_head(struct list_head *head, char *s)
-{
-    element_t *new = malloc(sizeof(element_t));
-    if (!new)
-        return false;
-
-    new->value = strdup(s);
-    if (!new->value) {
-        free(new);
-        return false;
-    }
-    list_add(&new->list, head);
-    return true;
-}
-
-/* Insert an element at tail of queue */
-bool q_insert_tail(struct list_head *head, char *s)
-{
     element_t *new = malloc(sizeof(element_t));
     if (!new)
         return false;
@@ -86,9 +67,6 @@ bool q_insert_tail(struct list_head *head, char *s)
     }
     list_add_tail(&new->list, head);
     return true;
-}
-
-```
 }
 
 /* Remove an element from head of queue */