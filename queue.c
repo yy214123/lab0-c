af6de3e28b68ee5c990e559526c4b152d0188c9c
yy214123 <yy214123@gmail.com>
2024-04-06 23:23:52 +0800

    Refactor queue insertion functions for efficiency
    In the original implementation, both q_insert_head and q_insert_tail
    functions contained duplicated code for inserting elements into a queue,
    
    The update simplifies this by utilizing the q_insert_head function within
    q_insert_tail to insert an element at the tail of the queue. This is
    achieved by passing head->prev to q_insert_head, effectively inserting
    the new element right before the head.

--- queue.c
+++ queue.c
@@ -61,19 +61,7 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
-    if (!head)
-        return false;
-    element_t *new = malloc(sizeof(element_t));
-    if (!new)
-        return false;
-
-    new->value = strdup(s);
-    if (!new->value) {
-        free(new);
-        new = NULL;
-        return false;
-    }
-    list_add_tail(&new->list, head);
+    q_insert_head(head->prev, s);
     return true;
 }
 
