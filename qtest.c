d3007447c34e75e9c5e3bf73d163bfd27cd8e4bc
yy214123 <yy214123@gmail.com>
2024-04-10 00:33:36 +0800

    Add command to qtest program
    This commit introduces the ttt command to the qtest program.
    The implementation involves adding a new function, do_ttt,
    which utilizes the fork-exec-wait pattern to execute the "ttt" game.

--- qtest.c
+++ qtest.c
@@ -1097,6 +1097,24 @@ static bool do_shuffle(int argc, char *argv[])
     return true;
 }
 
+static bool do_ttt(int argc, char *argv[])
+{
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        char *args[] = {"./ttt", NULL};
+        execvp(args[0], args);
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        int status;
+        waitpid(pid, &status, 0);
+    } else {
+        fprintf(stderr, "Failed to fork.\n");
+        return false;
+    }
+    return true;
+}
+
 static void console_init()
 {
     ADD_COMMAND(new, "Create new queue", "");
@@ -1141,6 +1159,7 @@ static void console_init()
     ADD_COMMAND(shuffle,
                 "Use the Fisher–Yates shuffle algorithm to implement shuffling",
                 "");
+    ADD_COMMAND(ttt, "Excute the ttt game", "");
     add_param("length", &string_length, "Maximum length of displayed string",
               NULL);
     add_param("malloc", &fail_probability, "Malloc failure probability percent",
