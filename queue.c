5fe306c402473de15143a428888e4264cad853c4
yy214123 <yy214123@gmail.com>
2025-02-26 20:00:50 +0800

    Implement q_free to properly free queue elements
    Previously, q_free() was defined but had no functionality.
    Now it correctly frees all elements in the queue and releases
    allocated memory.
    
    Change-Id: Id0d688bc368e94281885f6e5fca0d69aefd417f3

--- queue.c
+++ queue.c
@@ -15,7 +15,21 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+    if (list_empty(head)) {
+        free(head);
+        return;
+    }
+    struct list_head *node, *safe;
+    list_for_each_safe (node, safe, head) {
+        element_t *current = list_entry(node, element_t, list);
+        q_release_element(current);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
