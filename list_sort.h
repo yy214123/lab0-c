6abe21fb3a9b71e707d08080dcadaf20fdd8ef57
yy214123 <yy214123@gmail.com>
2025-03-08 01:36:29 +0800

    Add list_sort implementation
    This commit introduces list_sort.c and list_sort.h,
    providing a sorting implementation for linked lists.
    The build system is updated to compile list_sort.o.
    
    Change-Id: Ic76a3ef073ef170f6ce9e1c54987f5441c1b4a61

---
+++ list_sort.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_LIST_SORT_H
+#define _LINUX_LIST_SORT_H
+
+#include <linux/types.h>
+
+struct list_head;
+
+typedef int
+    __attribute__((nonnull(1, 2))) (*list_cmp_func_t)(const struct list_head *,
+                                                      const struct list_head *);
+
+__attribute__((nonnull(1, 2))) void list_sort(struct list_head *head,
+                                              list_cmp_func_t cmp);
+#endif
